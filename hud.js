c12e6a279445c12cb282e30f373ccb5146a4a6a4
Ben Mather <bwhmather@bwhmather.com>
2011-04-19 23:49:41 +0100

    Broke the HUD code down into objects associated with each component of the interface.

--- hud.js
+++ hud.js
@@ -1,22 +1,9 @@
-//==============================================================================
-// HUD
-//==============================================================================
-
 MG.hud = (function () {
-    var mRadarMissilePositionDot;
-    var mRadarMissileTargetDot;
-
-    var mSpeedometerSpeedText;
-    var mSpeedometerBar;
-
-    var mLevelIndicatorText;
-
-    var mProgressBar;
-    var mBestProgressBar;
-
-    var mLivesIndicatorMissiles;
-    var mLivesIndicatorInfinite;
-    var mLivesIndicatorNone;
+    var mRadar;
+    var mSpeedometer;
+    var mLevelIndicator;
+    var mProgressIndicator;
+    var mLifeCounter;
 
     return {
 
@@ -24,115 +11,176 @@ MG.hud = (function () {
             var rootNode = document.getElementById('hud');
 
             // ----------------------------------------------------------- Radar
-            mRadarMissilePositionDot = document.getElementById('hud-radar-scope-missile');
-            mRadarMissileTargetDot = document.getElementById('hud-radar-scope-missile-target');
+
+            mRadar = (function () {
+                var mMissilePositionDot = document.getElementById('hud-radar-scope-missile');
+                var mMissileTargetDot = document.getElementById('hud-radar-scope-missile-target');
+
+                var mMissileTarget = {x: 0.0, y: 0.0};
+                var mMissilePosition = {x: 0.0, y: 0.0};
+
+                return {
+                    update: function (dt) {
+                        mMissileTarget = MG.missile.getTarget();
+                        mMissilePosition = MG.missile.getPosition();
+                        // PASS
+                    },
+                    updateDOM: function () {
+                        var x,y;
+                        var scopeRadius = 0.5;
+
+                        /* Set the position of the dot indicating the intended target of the missile */
+                        x = scopeRadius + 0.95 * scopeRadius * mMissileTarget.x
+                                               / MG.TUNNEL_RADIUS;
+                        y = scopeRadius + 0.95 * scopeRadius * mMissileTarget.y
+                                               / MG.TUNNEL_RADIUS;
+
+                        mMissileTargetDot.setAttribute('cx', String(x));
+                        mMissileTargetDot.setAttribute('cy', String(y));
+
+                        /* Set the position of the dot indicating the actual position of the missile */
+                        x = scopeRadius + 0.95 * scopeRadius * mMissilePosition.x
+                                               / MG.TUNNEL_RADIUS;
+                        y = scopeRadius + 0.95 * scopeRadius * mMissilePosition.y
+                                               / MG.TUNNEL_RADIUS;
+
+                        mMissilePositionDot.setAttribute('cx', String(x));
+                        mMissilePositionDot.setAttribute('cy', String(y));
+                    }
+                };
+            }());
+
 
 
             // ----------------------------------------------------- Speedometer
-            mSpeedometerSpeedText = document.createTextNode('');
-            document.getElementById('hud-speedometer-speed-text').appendChild(mSpeedometerSpeedText);
+
+            mSpeedometer = (function () {
+                var mBarNode = document.getElementById('hud-speedometer-bar');
+
+                var mTextNode = document.createTextNode('');
+                document.getElementById('hud-speedometer-speed-text').appendChild(mTextNode);
             
-            mSpeedometerBar = document.getElementById('hud-speedometer-bar');
-          
+                var mSpeed = 0.0;
+
+                return {
+                    update: function (dt) {
+                        mSpeed = MG.missile.getVelocity();
+                    },
+                    updateDOM: function () {
+                        mTextNode.data = Math.floor(mSpeed);
+                        
+                        // TODO (possibly) work out the maximum speed properly and put a cap on the level with a nice victory screen
+                        mBarNode.setAttribute('x', mSpeed/2000 - 1);
+                    }
+                };
+            } ());
 
             // ------------------------------------------------- Level Indicator
-            mLevelIndicatorText = document.createTextNode('');
-            document.getElementById('hud-level-indicator').appendChild(mLevelIndicatorText);
+            mLevelIndicator = (function () {
+                var mTextNode = document.createTextNode('');
+                document.getElementById('hud-level-indicator').appendChild(mTextNode);
+
+                return {
+                    update: function (dt) {
+                    // PASS
+                    },
+
+                    updateDOM: function () {
+                        mTextNode.data = MG.game.getLevelString();
+                    }
+                };
+            } ());
 
             // ---------------------------------------------- Progress Indicator
-            mProgressBar = document.getElementById('hud-progress-indicator-progress');
-            mBestProgressBar = document.getElementById('hud-progress-indicator-best-progress');
+            mProgressIndicator = (function () {
+                var mProgressMarkNode = document.getElementById('hud-progress-indicator-progress');
+                var mBestProgressMarkNode = document.getElementById('hud-progress-indicator-best-progress');
 
+                var mProgress = 0.0;
+                var mBestProgress = 0.0;
+
+                return {
+                    update: function (dt) {
+                        mProgress = MG.game.getProgress();
+                        mBestProgress = MG.game.getBestProgress();
+                    },
+
+                    updateDOM: function () {
+                        mProgressMarkNode.setAttribute('transform', 'translate(0,'+mProgress+')');
+                        mBestProgressMarkNode.setAttribute('transform', 'translate(0,'+mBestProgress+')');
+                    }
+                };
+            } ());
             // ---------------------------------------------------- Life Counter
-            mLivesIndicatorInfinite = document.getElementById('hud-lives-indicator-infinite');
-            mLivesIndicatorNone = document.getElementById('hud-lives-indicator-none');
 
-            mLivesIndicatorMissiles = [];
-            for (var i=0; i<5; i++) {
-                mLivesIndicatorMissiles[i] = document.getElementById('hud-lives-indicator-missile-' + (i+1));
-            }
+            mLifeCounter = (function () {
+                var i;
+
+                var mInfiniteLivesNode = document.getElementById('hud-lives-indicator-infinite');
+                var mZeroLivesNode = document.getElementById('hud-lives-indicator-none');
+
+                var mLivesNodes = [];
+                for (i=0; i<5; i++) {
+                    mLivesNodes[i] = document.getElementById('hud-lives-indicator-missile-' + (i+1));
+                }
+
+                var mLives = -1;
+
+                return {
+                    update: function (dt) {
+                        mLives = MG.game.getNumLives();
+                    },
+
+                    updateDOM: function () {
+                        var i;
+
+                        switch (mLives) {
+                          case Infinity:
+                            mInfiniteLivesNode.setAttribute('visibility', 'visible');
+                            mZeroLivesNode.setAttribute('visibility', 'hidden');
+
+                            for (i=0; i<5; i++) {
+                                mLivesNodes[i].setAttribute('visibility', 'hidden');
+                            }
+                            break;
+                          case 0:
+                            mInfiniteLivesNode.setAttribute('visibility', 'hidden');
+                            mZeroLivesNode.setAttribute('visibility', 'visible');
+
+                            for (i=0; i<5; i++) {
+                                mLivesNodes[i].setAttribute('visibility', 'hidden');
+                            }
+                            break;
+                          default:
+                            mInfiniteLivesNode.setAttribute('visibility', 'hidden');
+                            mZeroLivesNode.setAttribute('visibility', 'hidden');
+
+                            for (i=0; i<5; i++) {
+                                mLivesNodes[i].setAttribute('visibility', i<mLives ? 'visible' : 'hidden');
+                            }
+                            break;
+                        }
+                    }
+                };
+            } ());
 
             rootNode.setAttribute('visibility', 'visible');
         },
 
-        updateDOM: function (dt) {
-
-            // ----------------------------------------------------------- Radar
-
-            var x,y;
-
-            var scopeRadius = 0.5;
-            var missileTarget = MG.missile.getTarget();
-
-            x = scopeRadius + 0.95 * scopeRadius * missileTarget.x
-                                   / MG.TUNNEL_RADIUS;
-            y = scopeRadius + 0.95 * scopeRadius * missileTarget.y
-                                   / MG.TUNNEL_RADIUS;
-
-            mRadarMissileTargetDot.setAttribute('cx', String(x));
-            mRadarMissileTargetDot.setAttribute('cy', String(y));
-
-
-            var missilePosition = MG.missile.getPosition();
-
-            x = scopeRadius + 0.95 * scopeRadius * missilePosition.x
-                                   / MG.TUNNEL_RADIUS;
-            y = scopeRadius + 0.95 * scopeRadius * missilePosition.y
-                                   / MG.TUNNEL_RADIUS;
-
-            mRadarMissilePositionDot.setAttribute('cx', String(x));
-            mRadarMissilePositionDot.setAttribute('cy', String(y));
-
-            // ----------------------------------------------------- Speedometer
-            var speed = MG.missile.getVelocity();
-            mSpeedometerSpeedText.data = Math.floor(speed);
-            
-            // TODO work out the maximum speed properly and put a cap on the level with a nice victory screen
-            mSpeedometerBar.setAttribute('x', speed/2000 - 1);
-
-            // ------------------------------------------------- Level Indicator
-            mLevelIndicatorText.data = MG.game.getLevelString();
-
-            // ---------------------------------------------- Progress Indicator
-            var progress = MG.game.getProgress();
-            mProgressBar.setAttribute('transform', 'translate(0,'+progress+')');
-
-            var bestProgress = MG.game.getBestProgress();
-            mBestProgressBar.setAttribute('transform', 'translate(0,'+bestProgress+')');
-
-
-            // ---------------------------------------------------- Life Counter
-            var lives = MG.game.getNumLives();
-            switch (lives) {
-              case Infinity:
-                mLivesIndicatorInfinite.setAttribute('visibility', 'visible');
-                mLivesIndicatorNone.setAttribute('visibility', 'hidden');
-
-                for (var i=0; i<5; i++) {
-                    mLivesIndicatorMissiles[i].setAttribute('visibility', 'hidden');
-                }
-                break;
-              case 0:
-                mLivesIndicatorInfinite.setAttribute('visibility', 'hidden');
-                mLivesIndicatorNone.setAttribute('visibility', 'visible');
-
-                for (var i=0; i<5; i++) {
-                    mLivesIndicatorMissiles[i].setAttribute('visibility', 'hidden');
-                }
-                break;
-              default:
-                mLivesIndicatorInfinite.setAttribute('visibility', 'hidden');
-                mLivesIndicatorNone.setAttribute('visibility', 'hidden');
-
-                for (var i=0; i<5; i++) {
-                    mLivesIndicatorMissiles[i].setAttribute('visibility', i<lives ? 'visible' : 'hidden');
-                }
-                break;
-            }
+        update: function (dt) {
+            mRadar.update(dt);
+            mSpeedometer.update(dt);
+            mLevelIndicator.update(dt);
+            mProgressIndicator.update(dt);
+            mLifeCounter.update(dt);
         },
 
-        update: function () {
-            // TODO
+        updateDOM: function () {
+            mRadar.updateDOM();
+            mSpeedometer.updateDOM();
+            mLevelIndicator.updateDOM();
+            mProgressIndicator.updateDOM();
+            mLifeCounter.updateDOM();
         }
     };
 }());
