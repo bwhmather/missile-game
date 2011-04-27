26abfb599f210f402b2c9a606e2868571df9432f
Ben Mather <bwhmather@bwhmather.com>
2011-04-27 21:41:10 +0000

    Fixed problem with Firefox not moving the banner when the DOM was changed by removing and reinserting the banner node at each update.

--- banner.js
+++ banner.js
@@ -62,7 +62,25 @@ MG.banner = (function () {
         },
 
         updateDOM: function () {
-            mRootNode.setAttribute('width', (30 + 80*(0.5 + 0.5*Math.cos(Math.PI*mVisibility))) + '%');
+
+
+            if (mVisibility === 0) {
+                mRootNode.setAttribute('visibility', 'hidden');
+            } else {
+                /* For some reason Firefox ignores changes to 'width' unless the
+                node is removed and reinserted */
+                /* Apparently removing and reinserting nodes is also necessary
+                for decent performance in Firefox but I have not done any testing */  
+                nextSibling = mRootNode.nextSibling ;
+                parent = mRootNode.parentNode;
+                parent.removeChild(mRootNode);
+
+                mRootNode.setAttribute('width', (30 + 80*(0.5 + 0.5*Math.cos(Math.PI*mVisibility))) + '%');
+                mRootNode.setAttribute('visibility', 'visible');
+
+                parent.insertBefore(mRootNode, nextSibling);
+            }
+
         },
 
 
