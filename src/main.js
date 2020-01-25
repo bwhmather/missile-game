91750c199d331e97c343785f2c15bc43bc737a65
Ben Mather <bwhmather@bwhmather.com>
2020-01-25 15:47:45 +0000

    Wait until end of click event before closing menu

--- src/main.js
+++ src/main.js
@@ -11,8 +11,7 @@ MG.init = function () {
             MG.game.onMouseMove(evt.clientX || evt.touches[0].clientX, evt.clientY || evt.touches[0].clientY);
         }, false);
 
-    window.addEventListener('mousedown', MG.game.onMouseClick, false);
-    window.addEventListener('touchstart', MG.game.onMouseClick, false);
+    window.addEventListener('click', MG.game.onMouseClick, false);
 
     var update = function (dt) {
         MG.fog.update(dt);
